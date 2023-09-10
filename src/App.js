12999cab2eaf81e8fab31140cb74e845c296951c
anas ismail <msanasismail@gmail.com>
2023-09-10 17:50:57 +0530

    all changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MailForm from './components/MailForm'
 
 const App = () => {
   return (
     <div className='appContainer'>
-      <Router>
-        <Routes>
-          <Route path='/' Component={MailForm} />
-        </Routes>
-      </Router>
+      <MailForm />
     </div>
   )
 }
